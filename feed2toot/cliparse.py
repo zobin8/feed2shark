4602d4d9dc4c33ecee8e7b0e571bb216daf54d72
Carl Chenet <chaica@ohmytux.com>
2019-12-31 12:35:00 +0100

    bump version and update CHANGELOG

--- feed2toot/cliparse.py
+++ feed2toot/cliparse.py
@@ -24,7 +24,7 @@ import os
 import os.path
 import sys
 
-__version__ = '0.13'
+__version__ = '0.14'
 
 class CliParse:
     '''CliParse class'''
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ CLASSIFIERS = [
 
 setup(
     name='feed2toot',
-    version='0.13',
+    version='0.14',
     license='GNU GPL v3',
     description='Parse rss feeds and send new posts to Mastodon',
     long_description='Parse rss feeds and send new posts to the Mastodon social network',
