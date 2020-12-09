a8958f914b5b2fb7faaf2a8633d94ae3d39221ef
Carl Chenet <chaica@ohmytux.com>
2020-12-09 01:29:25 +0100

    bump version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ CLASSIFIERS = [
 
 setup(
     name='feed2toot',
-    version='0.15',
+    version='0.16',
     license='GNU GPL v3',
     description='Parse rss feeds and send new posts to Mastodon',
     long_description='Parse rss feeds and send new posts to the Mastodon social network',
